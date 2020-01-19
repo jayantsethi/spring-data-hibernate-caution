a3b0c8f8270f53c4f54d2b2dd9dfa308067883f3
Jayant Sethi <thejayantsethi@gmail.com>
2020-01-19 22:32:34 +0530

    Use entity graph to solve n+1 query problem
    You can now see from the sql logs that there's just one
    query fired to fetch all the posts along with their
    comments
    
    Note that entity graph won't work if there are more than one
    List type attribute in it. In such a case, use Set instead
    of list, if possible.
    
    In scenarios where above solution is not possible, then you
    can always fall back on writing your own query to fetch
    all the required data.

--- src/main/java/com/example/springdata/repository/PostRepository.java
+++ src/main/java/com/example/springdata/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.example.springdata.repository;
 
 import com.example.springdata.entity.Post;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends CrudRepository<Post, Integer> {
+    @Override
+    @EntityGraph(attributePaths = {"comments"})
+    Iterable<Post> findAll();
 }
